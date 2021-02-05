e4d5af1c205fa5a240afe90c5bda7c98c0819fd4
longbiu <longbiu.liao@anker.com>
2021-02-05 14:36:40 +0800

    成功整合了pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_05_013808) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
