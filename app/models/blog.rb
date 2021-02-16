24051856cbbd89d14ddabfc98ca8e79d6fb3e20c
longbiu <longbiu.liao@anker.com>
2021-02-16 19:17:44 +0800

    add user length mininum 3

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,7 @@
 class Blog < ApplicationRecord
   has_many :comments
   mount_uploader :picture, PictureUploader
+  belongs_to :user
   validates :title, presence: true,
              length: { minimum: 5 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, length: {minimum: 3}
+  has_many :blogs
 end
