c3903dfd7e6805c7e78d1bf35e99fc1487a0d6eb
longbiu <longbiu.liao@anker.com>
2021-02-20 09:43:09 +0800

    添加管理员功能前的准备

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  validates :username, length: {minimum: 3},
-            uniqueness: {case_sensitive: false}
-  has_secure_password
-  validates :password, presence: true,length: {minimum: 6}
-  has_many :blogs
-end
